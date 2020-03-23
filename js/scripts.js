ff94963a1aecce19cb98ca57f752db0d1e1f2ee6
Tyler Bates <Fondlytyler@gmail.com>
2020-03-23 11:27:45 -0700

    add subtract,multipl, and division functions

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,31 @@ var add = function(number1, number2) {
   var number2 = parseInt(prompt("Enter a number:"));
   
   alert(add(number1, number2));
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+  };
+
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter a number:"));
   
+  alert(subtract(number1, number2));
+  
+var multiply = function(number1, number2) {
+    return number1 * number2;
+  };
+
+  var number1 = parseInt(prompt("Enter a number"));
+  var number2 = parseInt(prompt("Enter a number"));
+  
+  alert(multiply(number1,number2));
+
+var divide = function(number1,number2) {
+  return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number"));
+var number2 = parseInt(prompt("Enter a number"));
+
+  alert(divide(number1,number2));
+
